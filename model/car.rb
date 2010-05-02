6cb82fb4df15e22beaf5e55effc82c0a71557533
Jonatan Kallus <jonatan@jonatan-kalluss-macbook.local>
2010-05-02 23:27:05 +0200

    cars move and transitions between paths, but breaks at end of map

--- model/car.rb
+++ model/car.rb
@@ -4,19 +4,33 @@ class Car
 	attr :next_path
 	attr :grid_pos
 
-  def initialize(path, distance, grid_pos)
+  def initialize(path, distance, grid_pos, tile_grid)
     @angle = 0
 		@current_path = path
 		@distance = distance
 		@grid_pos = grid_pos
-		@next_path = path #for now
+		@tile_grid = tile_grid
+		choose_next_path!
   end
 
 	def pos
 		pos = @current_path.point(@distance)
-		pos[0] += grid_pos[0] * Tile.width
-		pos[1] += grid_pos[1] * Tile.height
-		pos
+		local_to_global(pos)
+	end
+
+	def speed #(m/s)
+		10
+	end
+
+	def step!(time) #seconds
+		@distance += speed * time
+
+		if @distance > current_path.length then
+			@distance -= current_path.length
+			choose_next_grid_pos!
+			current_path = next_path
+			choose_next_path!
+		end
 	end
 
   class << self
@@ -28,4 +42,38 @@ class Car
       5
     end
   end
+
+	private
+	def choose_next_path!
+		end_point = current_path.point(current_path.length)
+		trans = grid_transition(end_point)
+		start_point = end_point
+		start_point[0] = 0 if end_point[0] == Tile.width
+		start_point[0] = Tile.width if end_point[0] == 0
+		start_point[1] = 0 if end_point[1] == Tile.height
+		start_point[1] = Tile.height if end_point[1] == 0
+		next_path = @tile_grid[grid_pos[1]+trans[1]][grid_pos[0]+trans[0]].paths(start_point)
+	end
+
+	def choose_next_grid_pos!
+		end_point = current_path.point(current_path.length)
+		grid_pos[0] += grid_transition(end_point)[0]
+		grid_pos[1] += grid_transition(end_point)[1]
+	end
+		
+	def local_to_global(pos)
+		[
+			pos[0] + grid_pos[0] * Tile.width,
+			pos[1] + grid_pos[1] * Tile.height
+		]
+	end
+
+	def grid_transition(end_point)
+		trans = [0,0]
+		trans[0] = 1 if end_point[0] == Tile.width
+		trans[0] = -1 if end_point[0] == 0
+		trans[1] = 1 if end_point[1] == Tile.height
+		trans[1] = -1 if end_point[1] == 0
+		trans
+	end
 end
--- model/model.rb
+++ model/model.rb
@@ -9,11 +9,19 @@ class Model
     @tile_grid.each_with_index do |tiles, y|
       tiles.each_with_index do |tile, x|
         start = tile.start_pos
-        @cars << Car.new(start[:path], start[:distance], [x,y])
+        @cars << Car.new(start[:path], start[:distance], [x,y], tile_grid)
+				break
       end
     end
   end
 
 	def step!
+		cars.each do |c|
+			c.step!(step_length)
+		end
+	end
+
+	def step_length #seconds
+		0.8
 	end
 end
