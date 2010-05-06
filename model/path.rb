7ded5ce5a54898301396d043ad621753ab9f5f9e
Joel Wilsson <joel.wilsson@gmail.com>
2010-05-06 18:50:46 +0200

    Fix indentation

--- model/path.rb
+++ model/path.rb
@@ -1,17 +1,17 @@
 class Path
-	attr_reader :length
+  attr_reader :length
   attr_reader :end_point
 
-	def initialize(parameterization, length, end_point)
-		@parameterization = parameterization
-		@length = length
+  def initialize(parameterization, length, end_point)
+    @parameterization = parameterization
+    @length = length
     @end_point = end_point
-	end
-
-	def point(distance)
-		@parameterization.call(distance) unless distance > length
-	end
-
+  end
+  
+  def point(distance)
+    @parameterization.call(distance) unless distance > length
+  end
+  
   def end_direction
     [end_point[0] == Tile.width ? 1 : (end_point[0] == 0 ? -1 : 0),
     end_point[1] == Tile.height ? 1 : (end_point[1] == 0 ? -1 : 0)]
